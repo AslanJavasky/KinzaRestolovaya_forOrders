f63ba2b930519522958c76302865f8efa742914b
AslanBolurov <aslanbolurov@list.ru>
2023-09-07 11:48:55 +0300

    finish

--- app/src/main/java/com/aslanbolurov/kinza/kinzarestolovaya/data/repoImpls/OrderRepoImpl.kt
+++ app/src/main/java/com/aslanbolurov/kinza/kinzarestolovaya/data/repoImpls/OrderRepoImpl.kt
@@ -6,6 +6,7 @@ import com.aslanbolurov.kinza.kinzarestolovaya.data.localDb.mapper.OrderMapper
 import com.aslanbolurov.kinza.kinzarestolovaya.data.utils.FirebaseUtils
 import com.aslanbolurov.kinza.kinzarestolovaya.domain.model.Order
 import com.aslanbolurov.kinza.kinzarestolovaya.domain.repo.OrderRepo
+import com.google.gson.Gson
 import java.text.SimpleDateFormat
 import java.util.Date
 import javax.inject.Inject
@@ -17,7 +18,9 @@ class OrderRepoImpl @Inject constructor(
 ):OrderRepo {
 
     override fun sendOrder(order: Order) {
-        firebaseUtils.dbRefence.push().setValue(order)
+        firebaseUtils.dbRefence.push().setValue(
+            Gson().toJson(order)
+        )
     }
 
     override suspend fun saveOrderToRoom(order: Order) {
